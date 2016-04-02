dd0cf367b01cde31813703882b5b7b606a36c034
Sergey Panfilov <sp@arex.io>
2016-04-03 00:06:27 +0800

    Refactor main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,42 @@
-var jsvat = (function () {
+var jsvat = (function (COUNTRIES) {
   'use strict';
+
+  //TODO (S.Panfilov) Fixed config and refactor
+  this.config = {
+    austria: true,
+    belgium: true,
+    bulgaria: true,
+    croatia: true,
+    cyprus: true,
+    czech_republic: true,
+    denmark: true,
+    estonia: true,
+    europe: true,
+    finland: true,
+    france: true,
+    germany: true,
+    greece: true,
+    hungary: true,
+    ireland: true,
+    italy: true,
+    latvia: true,
+    lithunia: true,
+    luxembourg: true,
+    malta: true,
+    netherlands: true,
+    norway: true,
+    poland: true,
+    portugal: true,
+    romania: true,
+    russia: true,
+    serbia: true,
+    slovakia_republic: true,
+    slovenia: true,
+    spain: true,
+    sweden: true,
+    switzerland: true,
+    united_kingdom: true
+  };
 
   function _validateRegex(vat, regex) {
     return regex.test(vat);
@@ -7,12 +44,9 @@ var jsvat = (function () {
 
   function _validateRules(vat, regex, countryName) {
     var parsedNum = regex.exec(vat);
-
-    //var code = parsedNum[1];
     var number = parsedNum[2];
-    //if (!code || code.length === 0) code = 'GB';
 
-    return _checks[countryName](number, countryName);
+    return COUNTRIES[countryName](number, countryName);
   }
 
   function _validate(vat, regex, countryName) {
@@ -23,40 +57,64 @@ var jsvat = (function () {
     return result;
   }
 
+  function getClearVAT(vat) {
+    return vat.toString().toUpperCase().replace(/(\s|-|\.)+/g, '');
+  }
+
+  function _makeArr(regex) {
+    //TODO (S.Panfilov) this is not cross-browser check
+    if (!Array.isArray(regex)) {
+      return [regex];
+    }
+
+    return regex;
+  }
+
   var exports = {
-    checkVAT: function (vat) {
+    checkVAT: function (vat, isDetailed) {
       if (!vat) return false;
-      vat = vat.toString().toUpperCase().replace(/(\s|-|\.)+/g, '');
 
-      for (var countryName in CONDITIONS) {
-        if (CONDITIONS.hasOwnProperty(countryName)) {
-          var regex = CONDITIONS[countryName].regex;
+      vat = getClearVAT(vat);
 
-          //TODO (S.Panfilov) this is not cross-browser check
-          if (Array.isArray(regex)) {
-            for (var i = 0; i < regex.length; i++) {
-              if (_validate(vat, regex[i], countryName)) return true;
+      var result = {
+        isValid: false,
+        countries: []
+      };
+
+      for (var countryName in COUNTRIES) {
+        if (COUNTRIES.hasOwnProperty(countryName)) {
+          var regexArr = _makeArr(COUNTRIES[countryName].regex);
+
+          for (var i = 0; i < regexArr.length; i++) {
+
+            //If once become a true, shouldn't be false any more
+            if (!result.isValid) {
+              result.isValid = _validate(vat, regexArr[i], countryName);
             }
-          } else {
-            if (_validate(vat, regex, countryName)) return true;
+
+            if (result.isValid) {
+              if (!isDetailed) {
+                return result.isValid
+              }
+              else {
+                result.push(countryName);
+              }
+
+            }
+
+
           }
         }
       }
-      return false;
+
+      return isDetailed ? result : result.isValid;
 
     }
   };
 
-  
-
-  var _checks = {
-    
-    
-    
-  };
 
   //Support of node.js
   if (typeof module === 'object' && module.exports) module.exports = exports;
 
   return exports;
-})();
+})(COUNTRIES);
