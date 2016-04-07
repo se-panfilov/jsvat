c411b6d0c84094189f7a1453bb66e2937315d365
Sergey Panfilov <sp@arex.io>
2016-04-07 11:00:41 +0800

    Fixes for tests

--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,16 @@
-module.exports ={
+var expect = require('chai').expect;
+var jsvat = require('../dist/jsvat.js');
 
+module.exports = {
+  check: function (arr, msg, isTrue, isDeepCheck) {
+    arr.forEach(function (item) {
+      return it(msg + ": " + item, function () {
+        if (isTrue) {
+          return expect(jsvat.checkVAT(item, isDeepCheck)).to.be.true;
+        } else {
+          return expect(jsvat.checkVAT(item, isDeepCheck)).to.be.flase;
+        }
+      });
+    });
+  }
 };
