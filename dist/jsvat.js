0a3f962dd61f3ba58b9e0cd89183d7882bf3a1be
Sergey Panfilov <sp@arex.io>
2016-04-22 00:31:33 +0800

    fixes

--- dist/jsvat.js
+++ dist/jsvat.js
@@ -58,7 +58,7 @@ var jsvat = (function() {
         if (COUNTRIES.hasOwnProperty(countryName)) {
 
           result.isValid = checkValidity(result.value, countryName);
-          result.country = countryName;
+          result.country = countryName; //TODO (S.Panfilov) BUG with country name in case of isValid===false always UK)
           if (result.isValid) return result;
         }
       }
