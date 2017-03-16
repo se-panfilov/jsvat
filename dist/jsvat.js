dd00647406bd4721ad171f72c9170f37769e902e
S.Panfilov <se-panfilov@ya.ru>
2017-03-16 17:27:44 +0800

    fixes

--- dist/jsvat.js
+++ dist/jsvat.js
@@ -12,7 +12,7 @@ var jsvat = (function() {
     var parsedNum = regex.exec(vat)
     var vatNum = parsedNum[2]
 
-    return ALL_COUNTRIES[countryName].calcs(vatNum)
+    return ALL_COUNTRIES[countryName].calcFn(vatNum)
   }
 
   function validate(vat, regex, countryName) {
@@ -28,7 +28,7 @@ var jsvat = (function() {
     return vat.toString().toUpperCase().replace(/(\s|-|\.)+/g, '')
   }
 
-  function _isCountryBlocked(config, countryName) {
+  function isCountryBlocked(config, countryName) {
     if (!config || config.length === 0) return false
 
     return config.indexOf(countryName) === -1
@@ -38,21 +38,11 @@ var jsvat = (function() {
     var regexArr = ALL_COUNTRIES[countryName].rules.regex
     for (var i = 0; i < regexArr.length; i++) {
       var isValid = validate(vat, regexArr[i], countryName)
-      if (isValid) return isValid && !_isCountryBlocked(exports.config, countryName)
+      if (isValid) return isValid && !isCountryBlocked(exports.config, countryName)
     }
     return false
   }
 
-  // function Country (name, codes, calcFn, rules) {
-  //   this.name = name
-  //   this.isoCode = {
-  //     short: codes[0],
-  //     long: codes[1],
-  //     numeric: codes[2]
-  //   }
-  //   this.calcFn = calcFn
-  //   this.rules = rules
-  // }
 
   function Result(vat, isValid, country) {
     this.value = vat || null
@@ -80,30 +70,27 @@ var jsvat = (function() {
 
     return false
   }
-  //
-  // function makeCountries (allCountries) {
-  //   var result = []
-  //   for (var k in allCountries) {
-  //     if (allCountries.hasOwnProperty(k)) {
-  //       // var isBlocked = isInList(blocked, allCountries[k])
-  //       // var isAllowed = isInList(allowed, allCountries[k])
-  //       // if (!isBlocked) {
-  //       //   if (isAllowed || allowed.length === 0) {
-  //       result.push(new Country(allCountries[k].name, allCountries[k].calcFn, allCountries[k].codes, allCountries.rules))
-  //       // }
-  //       // }
-  //     }
-  //   }
-  //
-  //   return result
-  // }
 
   function isBlocked(country, blocked, allowed) {
-    var isBlocked = isInList(this.blocked, country)
+    var isBlocked = isInList(blocked, country)
     if (isBlocked) return true
-    var isAllowed = isInList(this.allowed, country)
-    if (this.allowed.length > 0 && !isAllowed) return true
-    return false
+    var isAllowed = isInList(allowed, country)
+    return allowed.length > 0 && !isAllowed
+  }
+
+  function getCountry(vat, countries) {
+    var prefix = vat.match(/^[A-z]*/)
+
+    for (var i = 0; i < countries.length; i++) {
+      const country = countries[i]
+      if (prefix === country.codes[0] || prefix === country.codes[1] || prefix === country.codes[2]) return country
+    }
+
+    return null
+  }
+
+  function isValid(vat, country) {
+    return country.calcFn(vat)
   }
 
   var exports = {
@@ -114,7 +101,7 @@ var jsvat = (function() {
       var cleanVAT = removeExtraChars(vat)
       var result = new Result(cleanVAT)
 
-      var country = getCountry(cleanVAT)
+      var country = getCountry(cleanVAT, this.countries)
       if (isBlocked(country, this.blocked, this.allowed)) return result
 
       var isValid = isValid(cleanVAT, country)
@@ -126,11 +113,6 @@ var jsvat = (function() {
 
   exports.countries = ALL_COUNTRIES
 
-  // function init () {
-  //   exports.countries = makeCountries()
-  // }
-  //
-  // init()
 
   // eslint-disable-next-line no-undef
   COUNTRIES.austria = {
