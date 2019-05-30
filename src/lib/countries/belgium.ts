20ec7d32cd318ec0d834d72fb572bf83367699c8
Sergei Panfilov <se-panfilov@ya.ru>
2019-05-30 20:16:07 +0200

    lint fixes

--- src/lib/countries/belgium.ts
+++ src/lib/countries/belgium.ts
@@ -4,14 +4,11 @@ export const belgium: Country = {
   name: 'Belgium',
   codes: ['BE', 'BEL', '056'],
   calcFn: (vat: string): boolean  => {
-    if (vat.length === 9) {
-      vat = '0' + vat;
-    }
+    const newVat = (vat.length === 9) ? ('0' + vat) : vat;
+    if (Number(newVat.slice(1, 2)) === 0) return false;
 
-    if (Number(vat.slice(1, 2)) === 0) return false;
-
-    const check = (97 - Number(vat.slice(0, 8)) % 97);
-    return check === Number(vat.slice(8, 10));
+    const check = (97 - Number(newVat.slice(0, 8)) % 97);
+    return check === Number(newVat.slice(8, 10));
   },
   rules: {
     regex: [/^(BE)(0?\d{9})$/]
