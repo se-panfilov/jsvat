591cd545c155bb4bd004b7cf28841dd6370206b6
Sergei Panfilov <se-panfilov@ya.ru>
2019-05-31 11:15:03 +0200

    refactoring Czech Republic

--- dist/lib/countries/czechRepublic.js
+++ dist/lib/countries/czechRepublic.js
@@ -2,13 +2,15 @@ export const czechRepublic = {
     name: 'Czech Republic',
     codes: ['CZ', 'CZE', '203'],
     calcFn: (vat) => {
-        if (_isLegalEntities(vat, czechRepublic.rules))
-            return true;
-        if (_isIndividualType2(vat, czechRepublic.rules))
-            return true;
-        if (_isIndividualType3(vat, czechRepublic.rules))
-            return true;
-        return _isIndividualType1(vat, czechRepublic.rules);
+        const { rules } = czechRepublic;
+        const { multipliers, additional, lookup } = rules;
+        if (!multipliers)
+            return false;
+        if (!additional)
+            return false;
+        if (!Array.isArray(multipliers))
+            return false;
+        return isLegalEntities(vat, multipliers, additional) || isIndividualType2(vat, multipliers, additional, lookup) || isIndividualType3(vat, additional) || isIndividualType1(vat, additional);
     },
     rules: {
         multipliers: [8, 7, 6, 5, 4, 3, 2],
@@ -22,18 +24,12 @@ export const czechRepublic = {
         ]
     }
 };
-function _isLegalEntities(vat, rules) {
-    if (!rules.additional)
-        return false;
+function isLegalEntities(vat, multipliers, additional) {
     let total = 0;
-    if (rules.additional[0].test(vat)) {
-        if (!rules.multipliers)
-            return false;
-        if (!Array.isArray(rules.multipliers))
-            return false;
+    if (additional[0].test(vat)) {
         // Extract the next digit and multiply by the counter.
         for (let i = 0; i < 7; i++) {
-            total += Number(vat.charAt(i)) * rules.multipliers[i];
+            total += Number(vat.charAt(i)) * multipliers[i];
         }
         // Establish check digit.
         total = 11 - total % 11;
@@ -42,32 +38,23 @@ function _isLegalEntities(vat, rules) {
         if (total === 11)
             total = 1;
         // Compare it with the last character of the VAT number. If it's the same, then it's valid.
-        const expect = +vat.slice(7, 8);
+        const expect = Number(vat.slice(7, 8));
         return total === expect;
     }
     return false;
 }
-function _isIndividualType1(vat, rules) {
-    if (!rules.additional)
-        return false;
-    if (rules.additional[1].test(vat)) {
-        const temp = Number(vat.slice(0, 2));
-        return temp <= 62;
+function isIndividualType1(vat, additional) {
+    if (additional[1].test(vat)) {
+        return Number(vat.slice(0, 2)) <= 62;
     }
     return false;
 }
-function _isIndividualType2(vat, rules) {
-    if (!rules.additional)
-        return false;
+function isIndividualType2(vat, multipliers, additional, lookup) {
     let total = 0;
-    if (rules.additional[2].test(vat)) {
-        if (!rules.multipliers)
-            return false;
-        if (!Array.isArray(rules.multipliers))
-            return false;
+    if (additional[2].test(vat)) {
         // Extract the next digit and multiply by the counter.
         for (let j = 0; j < 7; j++) {
-            total += +vat.charAt(j + 1) * rules.multipliers[j];
+            total += Number(vat.charAt(j + 1)) * multipliers[j];
         }
         // Establish check digit.
         total = 11 - total % 11;
@@ -77,16 +64,14 @@ function _isIndividualType2(vat, rules) {
             total = 1;
         // Convert calculated check digit according to a lookup table
         const expect = Number(vat.slice(8, 9));
-        if (!rules.lookup)
+        if (!lookup)
             return false;
-        return rules.lookup[total - 1] === expect;
+        return lookup[total - 1] === expect;
     }
     return false;
 }
-function _isIndividualType3(vat, rules) {
-    if (!rules.additional)
-        return false;
-    if (rules.additional[3].test(vat)) {
+function isIndividualType3(vat, additional) {
+    if (additional[3].test(vat)) {
         const temp = Number(vat.slice(0, 2)) + Number(vat.slice(2, 4)) + Number(vat.slice(4, 6)) + Number(vat.slice(6, 8)) + Number(vat.slice(8));
         const expect = Number(vat) % 11 === 0;
         return !!(temp % 11 === 0 && expect);
