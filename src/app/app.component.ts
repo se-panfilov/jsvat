89bc32923d75470164f92f0df0e8e044e47bd6c9
S.Panfilov <se-panfilov@ya.ru>
2019-06-08 13:10:54 +0200

    experimetns with three shaking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup } from '@angular/forms'
 import {
-  austria,
-  belgium,
-  bulgaria,
   checkVAT,
-  croatia,
-  cyprus,
-  czechRepublic,
-  denmark,
-  estonia,
-  europe,
-  finland,
-  france,
-  germany,
-  greece,
-  hungary,
-  ireland,
-  italy,
-  latvia,
-  lithuania,
-  luxembourg,
-  malta,
-  netherlands,
-  norway,
-  poland,
-  portugal,
-  romania,
-  russia,
-  serbia,
-  slovakiaRepublic,
-  slovenia,
-  spain,
-  sweden,
-  switzerland,
-  unitedKingdom,
+  countries,
   VatCheckResult
 } from 'jsvat'
 
@@ -63,41 +31,7 @@ export class AppComponent implements OnInit {
   }
 
   updateExampleValue(vat: string | undefined): void {
-    this.exampleResult = checkVAT(vat, [
-      austria,
-      belgium,
-      bulgaria,
-      switzerland,
-      cyprus,
-      czechRepublic,
-      germany,
-      denmark,
-      greece,
-      spain,
-      europe,
-      estonia,
-      finland,
-      france,
-      unitedKingdom,
-      croatia,
-      hungary,
-      ireland,
-      italy,
-      latvia,
-      lithuania,
-      luxembourg,
-      malta,
-      netherlands,
-      norway,
-      poland,
-      portugal,
-      romania,
-      russia,
-      serbia,
-      slovenia,
-      slovakiaRepublic,
-      sweden
-    ])
+    this.exampleResult = checkVAT(vat, countries)
   }
 
 
