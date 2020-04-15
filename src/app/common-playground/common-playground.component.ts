340cfd8ebc1dba3fff7c7d077e27cca01dd97704
Sergei <se-panfilov@ya.ru>
2020-04-15 13:43:00 +0200

    make vats clickable

--- src/app/common-playground/common-playground.component.ts
+++ src/app/common-playground/common-playground.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup } from '@angular/forms'
-import { checkVAT, Country } from 'jsvat'
-import { highlight, languages } from 'prismjs'
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { checkVAT, Country } from 'jsvat';
+import { highlight, languages } from 'prismjs';
 
 @Component({
   selector: 'jsvat-common-playground',
@@ -10,14 +10,14 @@ import { highlight, languages } from 'prismjs'
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CommonPlaygroundComponent implements OnInit {
-  public code: string | undefined
-  public panelOpenState = false
+  public code: string | undefined;
+  public panelOpenState = false;
 
-  public countries: ReadonlyArray<Country> = []
+  public countries: ReadonlyArray<Country> = [];
 
   readonly form: FormGroup = this.fb.group({
     vat: ''
-  })
+  });
 
   constructor(private fb: FormBuilder) {
   }
@@ -25,19 +25,23 @@ export class CommonPlaygroundComponent implements OnInit {
   ngOnInit(): void {
     this.form.get('vat')
       .valueChanges
-      .subscribe(vat => this.updateExampleValue(vat, this.countries))
+      .subscribe(vat => this.updateExampleValue(vat, this.countries));
 
-    this.form.setValue({ vat: 'ATU00000024' })
+    this.form.setValue({ vat: 'ATU00000024' });
   }
 
   updateExampleValue(vat: string | undefined, countries: ReadonlyArray<Country>): void {
-    const code = checkVAT(vat, countries)
-    const codeStr = JSON.stringify(code, null, 2).replace(/"(\S+)":/g, '$1:')
-    if (code) this.code = highlight(codeStr, languages.javascript, 'javascript')
+    const code = checkVAT(vat, countries);
+    const codeStr = JSON.stringify(code, null, 2).replace(/"(\S+)":/g, '$1:');
+    if (code) this.code = highlight(codeStr, languages.javascript, 'javascript');
   }
 
   onCountriesListUpdated(list: ReadonlyArray<Country>): void {
-    this.countries = list
-    this.updateExampleValue(this.form.get('vat').value, this.countries)
+    this.countries = list;
+    this.updateExampleValue(this.form.get('vat').value, this.countries);
+  }
+
+  setCode(vat: string): void {
+    this.form.get('vat').patchValue(vat);
   }
 }
