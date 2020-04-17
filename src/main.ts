23e24037a7d72ceafc6d77c1ea39ef9cf71ae077
Sergei <se-panfilov@ya.ru>
2020-04-17 11:13:17 +0200

    output version of the app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { version } from '../package.json';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -10,3 +10,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+if (window) (window as any).ghpVersion = version;
